75a2b50a9461e5334d0fc71fd7d2517949ebc2ad
Hchan2 <seongchan_@naver.com>
2023-06-21 17:40:42 +0900

    NotFound 페이지 추가, README 작성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import MainPage from './components/MainPage';
+import NotFound from './components/NotFound';
 import Nav from './nav/Nav';
 import SignIn from './user/SignIn'
 import SignUp from './user/SignUp'
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
---
+++ src/components/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FlexComponent, MainImage } from '../style/styled-components'
+
+const NotFound = () => {
+    return (
+        <FlexComponent>
+            <MainImage
+                src='https://img.freepik.com/free-vector/page-found-concept-illustration_114360-1869.jpg'
+                loading='lazy'
+            />
+        </FlexComponent>
+    )
+}
+
+export default NotFound
